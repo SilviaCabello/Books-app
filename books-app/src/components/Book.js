a76cb08fb85c2d7e6435fd0b13423e3fa87db510
SilviaCabello <silviacabellom@gmail.com>
2021-05-02 19:24:29 +0200

    add title and author to the book component

--- books-app/src/components/Book.js
+++ books-app/src/components/Book.js
@@ -1,7 +1,11 @@
-const Book = ({ image }) => {
-    return (
-        <img src={image} alt=""/>
-    )
+const Book = ({ image, title, author }) => {
+  return (
+    <div>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </div>
+  );
 };
 
 export default Book;
